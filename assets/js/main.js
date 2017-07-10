4c1b196464f27262e33cf0e0bd2b21769f1f98da
Bex Way <rebeccaway2016@u.northwestern.edu>
2017-07-10 15:47:42 -0500

    update - added styling for navbar and header, media icons, and hover on navbar items

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,18 @@
 window.onload = function() {
-  addClasses("navbar", ["flex", "flex-around", "bg-accent-medium", "font-light"]);
-  addClasses("navbar-item", ["inline"]);
-  addClasses("content", ["bg-light"]);
   addClasses("bio-img", ["size-small", "br-round"]);
-  addClasses("home-header", ["display-inline-block"]);
-  addClasses("home-description", ["flex", "flex-left"]);
-  addClasses("resume-page", ["hidden"]);
+  addClasses("content", ["bg-light", "padding-thin"]);
+  addClasses("font-general", ["fontsize-general"]);
+  addClasses("header", ["bg-accent-dark", "font-light", "padding-thin", "flex", "flex-between", "flex-vcenter"]);
+  addClasses("header-icon", ["icon-small"]);
+  addClasses("header-subsec", ["display-inline"]);
+  addClasses("home-description", ["flex", "flex-center", "flex-wrap"]);
+  addClasses("home-page", ["hidden"]);
+  addClasses("navbar", ["flex", "flex-around", "bg-accent-medium", "font-light", "fontsize-general"]);
+  addClasses("navbar-item", ["inline", "size-full", "font-center", "padding-thin"]);
   addClasses("projects-page", ["hidden"]);
+  addClasses("font-title", ["fontsize-title", "font-lineheight-title"]);
+  addClasses("font-small", ["fontsize-small", "font-italics"]);
+  addClasses("font-subtitle", ["fontsize-subtitle", "font-lineheight-title"]);
 
 };
 
@@ -18,3 +24,7 @@ function addClasses(addTo, classesList){
       elements[i].className += " " + classesToAdd;
   }
 }
+
+
+// https://stackoverflow.com/questions/32421616/how-to-remove-class-with-media-queries
+// https://www.sitepoint.com/javascript-media-queries/
