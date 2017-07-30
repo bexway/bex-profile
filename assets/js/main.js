1387ba7ae2a80a65861cbd987ac4f4c6334c0145
Bex Way <rebeccaway2016@u.northwestern.edu>
2017-07-29 23:33:30 -0500

    update - restructured resume section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,23 @@
 window.onload = function() {
+  addClasses("about-description", ["flex", "flex-center", "flex-wrap"]);
+  // addClasses("about-page", ["hidden"]);
   addClasses("bio-img", ["size-small", "br-round"]);
   addClasses("content", ["bg-light", "padding-thin"]);
+  addClasses("font-content-title", ["fontsize-title", "font-lineheight-title"]);
   addClasses("font-general", ["fontsize-general"]);
-  addClasses("header", ["bg-accent-dark", "font-light", "padding-thin", "flex", "flex-between", "flex-vcenter"]);
-  addClasses("header-icon", ["icon-small"]);
-  addClasses("header-subsec", ["display-inline"]);
-  addClasses("home-description", ["flex", "flex-center", "flex-wrap"]);
-  addClasses("home-page", ["hidden"]);
-  addClasses("navbar", ["flex", "flex-around", "bg-accent-medium", "font-light", "fontsize-general"]);
-  addClasses("navbar-item", ["inline", "size-full", "font-center", "padding-thin"]);
-  addClasses("projects-page", ["hidden"]);
+  addClasses("font-resume-subtitle", ["fontsize-general", "fontstyle-bold"]);
   addClasses("font-title", ["fontsize-title", "font-lineheight-title"]);
   addClasses("font-small", ["fontsize-small", "font-italics"]);
   addClasses("font-subtitle", ["fontsize-subtitle", "font-lineheight-title"]);
+  addClasses("header", ["bg-accent-dark", "font-light", "padding-thin", "flex", "flex-between", "flex-vcenter"]);
+  addClasses("header-icon", ["icon-small"]);
+  addClasses("header-subsec", ["display-inline"]);
+  addClasses("navbar", ["flex", "flex-around", "bg-accent-medium", "font-light", "fontsize-general"]);
+  addClasses("navbar-item", ["inline", "size-full", "font-center", "padding-thin"]);
+
+
+  addClasses("about-page", ["hidden"]);
+  addClasses("projects-page", ["hidden"]);
 
 };
 
